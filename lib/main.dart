716fac1987d188eb5e50dd3fffd1596396566daa
Krishan <krishan.20212035@iit.ac.lk>
2024-03-24 18:31:05 +0530

    Deleted unecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,182 +1,65 @@
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'package:safedrive/common/theme.dart';
-// import 'package:safedrive/screens/Home.dart';
-// import 'package:safedrive/services/services.dart';
-
-// import 'firebase_options.dart';
-// import 'screens/screens.dart';
-
-// // Lisitnening to the background messages
-// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   final NotificationService notificationService = NotificationService();
-//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-//   await notificationService.setupFlutterNotifications();
-//   notificationService.showFlutterNotification(message);
-//   print('Handling a background message ${message.messageId}');
-// }
-
-// Future<void> main() async {
-//   await dotenv.load(fileName: "assets/.env");
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-
-//   // Listening to the background messages
-//   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-//   if (!kIsWeb) {
-//     await NotificationService().setupFlutterNotifications();
-//   }
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Safe Drive',
-//       debugShowCheckedModeBanner: false,
-//       theme: light,
-//       themeMode: ThemeMode.light,
-//       routes: {
-//         '/welcome': (context) => const Welcome(),
-//         '/login': (context) => const Login(),
-//         '/register': (context) => const Register(),
-//         '/Home': (context) => const Home(),
-//       },
-//       initialRoute: '/welcome',
-//     );
-//   }
-// }
-
-// import 'package:flutter/material.dart';
-// import 'package:safe_drive/view/profile_view.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home:  const ProfileView(),
-//     );
-//   }
-// }
-//
-
-// var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
-
-//Krishan
-// main.dart
-
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:safedrive/firebase_options.dart';
-// import 'package:safedrive/screens/Home.dart';
-
-// import 'package:safedrive/services/auth/auth_gate.dart'; // Updated import
-// import 'package:safedrive/services/auth/auth_service.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-//   runApp(ChangeNotifierProvider(
-//     create: (context) => AuthService(),
-//     child: const MyApp(),
-//   ));
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Home(), // No need to use AuthGate anymore, directly set HomePage
-//     );
-//   }
-// }
-
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:safedrive/firebase_options.dart';
-
-// import 'package:safedrive/services/auth/auth_gate.dart';
-// import 'package:safedrive/services/auth/auth_service.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-//   runApp(ChangeNotifierProvider(
-//     create: (context) => AuthService(),
-//     child: const MyApp(),
-//   ));
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: AuthGate(),
-//     );
-//   }
-// }
-
-// main.dart
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:safedrive/firebase_options.dart';
-import 'package:safedrive/pages/home_page.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:safedrive/common/theme.dart';
 import 'package:safedrive/screens/Home.dart';
+import 'package:safedrive/services/services.dart';
 
-import 'package:safedrive/services/auth/auth_gate.dart'; // Updated import
-import 'package:safedrive/services/auth/auth_service.dart';
+import 'firebase_options.dart';
+import 'screens/screens.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+// Lisitnening to the background messages
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  final NotificationService notificationService = NotificationService();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(ChangeNotifierProvider(
-    create: (context) => AuthService(),
-    child: const MyApp(),
-  ));
+  await notificationService.setupFlutterNotifications();
+  notificationService.showFlutterNotification(message);
+  print('Handling a background message ${message.messageId}');
+}
+
+Future<void> main() async {
+  await dotenv.load(fileName: "assets/.env");
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Listening to the background messages
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  if (!kIsWeb) {
+    await NotificationService().setupFlutterNotifications();
+  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Safe Drive',
       debugShowCheckedModeBanner: false,
-      home: Home(), // No need to use AuthGate anymore, directly set HomePage
+      theme: light,
+      themeMode: ThemeMode.light,
+      routes: {
+        '/welcome': (context) => const Welcome(),
+        '/login': (context) => const Login(),
+        '/register': (context) => const Register(),
+        '/Home': (context) => const Home(),
+      },
+      initialRoute: '/welcome',
     );
   }
 }
+var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
+
+
+
+
+
